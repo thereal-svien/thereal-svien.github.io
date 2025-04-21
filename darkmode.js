c189ad1f631924007e9ef8c316eec3d8d22b8a14
svien <lime.thepup@gmail.com>
2025-04-21 16:51:44 +0800

    toilet

--- darkmode.js
+++ darkmode.js
@@ -4,9 +4,9 @@ const body = document.body;
 // Load saved mode
 if (localStorage.getItem("theme") === "dark") {
   body.classList.add("dark-mode");
-  toggleButton.textContent = "light mode";
-} else {
   toggleButton.textContent = "dark mode";
+} else {
+  toggleButton.textContent = "light mode";
 }
 
 toggleButton.addEventListener("click", () => {
@@ -15,9 +15,9 @@ toggleButton.addEventListener("click", () => {
   // Save preference and update icon
   if (body.classList.contains("dark-mode")) {
     localStorage.setItem("theme", "dark");
-    toggleButton.textContent = "light mode";
+    toggleButton.textContent = "dark mode";
   } else {
     localStorage.setItem("theme", "light");
-    toggleButton.textContent = "dark mode";
+    toggleButton.textContent = "light mode";
   }
 });
